6006790d71c1ea65d877918152087acdf73e83e2
Lorena Mellado <lorena.mellado@garajedeideas.com>
2023-10-04 09:03:17 +0200

    @Input

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
-  constructor() { }
+export class MainPageComponent {
 
-  ngOnInit() { }
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 200
+  }, {
+    name: 'Goku',
+    power: 400
+  }, {
+    name: 'Vegetta',
+    power: 1000
+  }]
+
 }
